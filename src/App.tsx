a6b3ebcc731857fa93a008b81d76c814c98cec04
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-02-11 21:32:12 -0500

    feat: add base project configuration

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Routes from './routes';
+
+function App(): React.ReactElement {
+  return <Routes />;
+}
+
+export default App;
---
+++ src/routes/MainRoutes.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+export const MainRoutes: object = {
+  path: '/',
+  element: <MainLayout />
+};
