3068b0ba4bdfd84994babb5f31e44908138d3189
VALENTIN <103033385+V-gitHub-17@users.noreply.github.com>
2023-03-24 22:26:34 -0300

    update

--- components/NicknameInput.tsx
+++ components/NicknameInput.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 export default function NicknameInput() {
-    // TODO: complete nickname input component logic and UI style
-  return (
-   
-<>
-<div className="centered-div">
-    <div className="header">
-        <h3>Your nickname is</h3>
-    </div>
-    <input type="text" pattern="[A-Za-z]+" required maxLength={8} title="Only letters allowed" placeholder="Tickypick" className="entername"/>
-</div>
-</>
+    return (
+        <>
+            <div className="centered-div">
+                <div className="header">
+                    <h3>Your nickname is</h3>
+                </div>
+                <input type="text" pattern="[A-Za-z]+" required maxLength={8} title="Only letters allowed" placeholder="Tickypick" className="entername" />
+            </div>
+        </>
     )
 }
 
