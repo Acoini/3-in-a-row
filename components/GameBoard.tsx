77cbbb0b4164d6d6a0194a2f33089cf15e66f2fd
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-03-26 14:36:30 -0500

    feat: center the game board

--- components/GameBoard.tsx
+++ components/GameBoard.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function GameBoard() {
   return (
-    <div className="grid grid-cols-3 gap-0 w-80 h-80 rounded-md overflow-hidden">
+    <div className="grid grid-cols-3 gap-0 w-80 h-80 rounded-md overflow-hidden shadow-md">
       {[...Array(9)].map((_, i) => (
         <div
           key={i}
-          className={`flex items-center justify-center bg-${i%2==0?'slate':'white'}-50`}
+          className={`flex items-center justify-center ${i%2==0?'bg-slate-50':'white'}`}
           onClick={() => console.log(i)}
         >
         </div>
--- pages/playroom.tsx
+++ pages/playroom.tsx
@@ -1,6 +1,19 @@
 import GameBoard from '@/components/GameBoard'
+import Head from 'next/head'
 import React from 'react'
 
 export default function Playroom() {
-  return <GameBoard />
+  return (
+    <>
+      <Head>
+        <title>Playroom</title>
+        <meta name="description" content="Playing high games" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='flex justify-center items-center h-screen'>
+        <GameBoard />
+      </main>
+    </>
+  )
 }
