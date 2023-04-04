11322f785c71171a26fb385eae1c21c699598cc0
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-04-04 08:54:52 -0500

    style: indent tag attributes

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,10 +40,27 @@ export default function Home() {
             <div className="header">
                 <h3>Your nickname is</h3>
             </div>
-            <input onChange={handleOnChange} value={nickname} type="text" pattern="[A-Za-z]+" required maxLength={8} title="Only letters allowed" placeholder="Tickypick" className="entername" />
+            <input 
+              onChange={handleOnChange} 
+              value={nickname} 
+              type="text" 
+              pattern="[A-Za-z]+" 
+              required maxLength={8} 
+              title="Only letters allowed" 
+              placeholder="Tickypick" 
+              className="entername" 
+            />
         </div>
         <Link href={'/playroom'}>
-          <Image onClick={handleStartNewGame} src='play.svg' className='w-auto h-auto' priority alt='Play button' width="21" height="21" />
+          <Image 
+            onClick={handleStartNewGame} 
+            src='play.svg' 
+            className='w-auto h-auto' 
+            priority 
+            alt='Play button' 
+            width="21" 
+            height="21" 
+          />
         </Link>
       </main>
     </>
