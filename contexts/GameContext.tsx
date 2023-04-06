b7e5136a1dd3bb4b0877e95860ce5902ed920fa8
VallecillaJesus <jesusvallecilla27@gmail.com>
2023-04-05 23:12:01 -0500

    refactor: change types folder location and change definitions

--- contexts/GameContext.tsx
+++ contexts/GameContext.tsx
@@ -1,56 +1,24 @@
 import React, { createContext, useState } from 'react'
+import { Game } from '@/@types/game'
 import { io } from 'socket.io-client'
 
 
-export const GameContext = createContext<GameContext>({} as GameContext)
+export const GameContext = createContext<any>({})
 
 const socket = io("http://localhost:5000")
 
 
 export default function GameProvider({ children }: any) {
-    const [currentGame, setCurrentGame] = useState<Game>({} as Game)
+    const [currentGame, setCurrentGame] = useState<Game>()
     
     socket.on("game", (args) => {
         console.log(args)
     })
 
-    function start(nickname: string) {
-        console.log(currentGame)
-        setCurrentGame({} as Game)
-        socket.emit("queue", nickname)
-    }
-
-    function move(position: number) {
-        let newGameState = currentGame.gameState 
-        newGameState[position] = currentGame.symbol
-        setCurrentGame({
-            ...currentGame,
-            gameState: newGameState 
-        })
-    }
-
-    const contextValues = {
-        currentGame,
-        setCurrentGame,
-        start,
-        move
-    }
-
     return (
-        <GameContext.Provider value={contextValues}>
+        <GameContext.Provider value={{}}>
             {children}
         </GameContext.Provider>
     )
 }
 
-interface GameContext {
-    currentGame: Game
-    start: Function
-}
-
-interface Game {
-    nickname: string 
-    symbol: string
-    gameState: string[] 
-    winner: string | null
-}
